81e359518fd127449a44c16b4eb8caaf3dc78e7c
Gregory Mensah <gregorymensah@Gregorys-MacBook-Air-3.local>
2023-04-26 22:08:51 -0400

    tyin maps

--- safetea/app/(tabs)/two.tsx
+++ safetea/app/(tabs)/two.tsx
@@ -1,14 +1,11 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { Dimensions, StyleSheet, View } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-
-export default function TabTwoScreen() {
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -16,16 +13,9 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
